c005acc67f06d7ade19b44151b238f7ff6de8623
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2024-03-09 13:38:27 +0900

    end syokyu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    movies_path status: 302
+  end
+
+  def after_sign_up_path_for(resource)
+    movies_path status: 302
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_user!, only: [:reservation]
   def index
     @movies = Movie.all
     if params[:is_showing] == '1'
